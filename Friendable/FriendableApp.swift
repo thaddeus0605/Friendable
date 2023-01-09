34033bf8473c996c4c3a4686537a5797fe68135f
thaddeus0605 <tdronski@gmail.com>
2023-01-09 11:17:08 -0600

    Initial Commit

---
+++ Friendable/FriendableApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendableApp.swift
+//  Friendable
+//
+//  Created by Thaddeus Dronski on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
